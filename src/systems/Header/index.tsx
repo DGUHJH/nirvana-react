d4404f210423c97b3ae1860aea5aeef0ad25460a
DGUHJH <ghkdwogud852@gmail.com>
2022-09-21 13:45:01 +0900

    Chore : Header 레이아웃 수정

--- src/systems/Header/index.tsx
+++ src/systems/Header/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
-import { InfoContainer, InfoTypo, InfoButton, LogoImg, Root } from './styled';
+import {
+  InfoContainer,
+  InfoTypo,
+  InfoButton,
+  LogoImg,
+  Root,
+  Container,
+  LogoContainer,
+  LogoTitleTypo,
+  LogoTypoContainer,
+  LogoDescriptionTypo,
+} from './styled';
 import logo from 'assets/images/logo.png';
 
 const Header = () => {
   return (
     <Root>
-      <LogoImg src={logo} />
-      <InfoContainer>
-        <InfoButton type='link'>
-          <InfoTypo>로그인</InfoTypo>
-        </InfoButton>
-      </InfoContainer>
+      <Container>
+        <LogoContainer>
+          <LogoImg src={logo} />
+          <LogoTypoContainer>
+            <LogoTitleTypo>니르바나 (LIRVANA)</LogoTitleTypo>
+            <LogoDescriptionTypo>동국대학교 게임 동아리</LogoDescriptionTypo>
+          </LogoTypoContainer>
+        </LogoContainer>
+        {/* <InfoContainer>
+          <InfoButton size='large'>
+            <InfoTypo>로그인</InfoTypo>
+          </InfoButton>
+        </InfoContainer> */}
+      </Container>
     </Root>
   );
 };
--- src/systems/Header/styled.ts
+++ src/systems/Header/styled.ts
@@ -4,15 +4,26 @@ import styled from 'styled-components';
 export const Root = styled.div`
   width: 100%;
   height: 100px;
-  background: #111;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 20px;
+  justify-content: center;
+  background: #fff;
+  border-bottom: 1px #111 solid;
   box-sizing: border-box;
   position: fixed;
   top: 0;
   left: 0;
+`;
+
+export const Container = styled.div`
+  width: 1280px;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+export const LogoContainer = styled.div`
+  display: flex;
+  gap: 20px;
 `;
 
 export const LogoImg = styled.img`
@@ -21,20 +32,34 @@ export const LogoImg = styled.img`
   border-radius: 20px;
 `;
 
+export const LogoTypoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+export const LogoTitleTypo = styled(Typography)`
+  &&& {
+    font-weight: 900;
+    font-size: 24px;
+  }
+`;
+
+export const LogoDescriptionTypo = styled(Typography)`
+  &&& {
+    font-weight: 350;
+    font-size: 16px;
+  }
+`;
+
 export const InfoContainer = styled.div`
   display: flex;
   gap: 10px;
 `;
 
-export const InfoButton = styled(Button)`
-  background: #0ff3;
-  border: 0;
-  border-radius: 10px;
-`;
+export const InfoButton = styled(Button)``;
 
 export const InfoTypo = styled(Typography)`
   &&& {
-    font-size: 18px;
-    color: white;
   }
 `;
