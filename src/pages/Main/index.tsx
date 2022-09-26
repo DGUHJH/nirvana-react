f866412e7fb689af12129b227ea517fda506b76a
DGUHJH <ghkdwogud852@gmail.com>
2022-09-26 17:45:06 +0900

    Feat : Main 페이지 user info 가져오게끔 수정

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,64 @@
-import React from 'react';
-import { Root } from './styled';
+import { commonAxios } from 'api/commonAxios';
+import React, { useEffect, useState } from 'react';
+import {
+  Container,
+  ContentButton,
+  ContentContainer,
+  ContentTypo,
+  Root,
+  TitleTypo,
+} from './styled';
 
 const Main = () => {
-  return <Root />;
+  const [name, setName] = useState<string>('');
+  const [score, setScore] = useState<number>();
+  const [isLogin, setIsLogin] = useState<boolean>();
+
+  const onClickLogout = () => {
+    localStorage.clear();
+    window.location.replace('/login');
+  };
+
+  useEffect(() => {
+    let uuid = localStorage.getItem('uuid');
+    setIsLogin(!!uuid);
+    if (uuid) {
+      commonAxios({
+        url: 'get_user_info.php',
+        params: {
+          uuid,
+        },
+        method: 'GET',
+      }).then((res) => {
+        if (res.data.type === 'success') {
+          setName(res.data.data.name);
+          setScore(res.data.data.score);
+        }
+      });
+    }
+  }, []);
+
+  return (
+    <Root>
+      <Container>
+        <TitleTypo level={2}>메인</TitleTypo>
+        <ContentContainer>
+          {isLogin ? (
+            <>
+              <ContentTypo>이름 : {name}</ContentTypo>
+              <ContentTypo>마일리지 점수 : {score}</ContentTypo>
+              <ContentButton type='primary' onClick={onClickLogout}>
+                로그아웃
+              </ContentButton>
+            </>
+          ) : (
+            <ContentButton type='primary' onClick={onClickLogout}>
+              로그인
+            </ContentButton>
+          )}
+        </ContentContainer>
+      </Container>
+    </Root>
+  );
 };
 export default Main;
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,3 +1,31 @@
+import { Button, Input, Typography } from 'antd';
 import styled from 'styled-components';
 
-export const Root = styled.div``;
+export const Root = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Container = styled.div`
+  width: 320px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-top: 20px;
+`;
+
+export const TitleTypo = styled(Typography.Title)``;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const ContentTypo = styled(Typography)``;
+
+export const ContentInput = styled(Input)``;
+
+export const ContentButton = styled(Button)``;
