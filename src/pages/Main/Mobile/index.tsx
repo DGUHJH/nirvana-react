7eed72ebfc3989c083867abe2fb7338bf2c9aaa0
DGUHJH <ghkdwogud852@gmail.com>
2022-10-04 11:14:06 +0900

    Feat : Mobile Main 활성화

--- src/pages/Main/Mobile/index.tsx
+++ src/pages/Main/Mobile/index.tsx
@@ -1,15 +1,184 @@
 import React, { useEffect, useState } from 'react';
+import MobileFooter from 'systems/Footer/Mobile';
 import MobileHeader from 'systems/Header/Mobile';
 import ItemCard from 'systems/ItemCard';
-import { Container, ContentContainer, Root } from './styled';
+import {
+  BannerImg,
+  Container,
+  ContentContainer,
+  FooterWrapper,
+  Root,
+  SwiperContainer,
+  SwiperInnerContainer,
+  SwiperTitleTypo,
+} from './styled';
+import banner from 'assets/images/main_banner.png';
+import useGoods from 'hooks/useGoods';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+import { Autoplay, FreeMode, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 const MobileMain = () => {
+  const { enterGoodsList, goodsList, allGoodsList, gengList, nirvanaList } =
+    useGoods();
+
   return (
     <Root>
+      <MobileHeader />
       <Container>
-        <MobileHeader />
-        <ContentContainer></ContentContainer>
+        <ContentContainer>
+          <BannerImg src={banner} />
+          <SwiperContainer>
+            <SwiperTitleTypo level={5}>응모 상품 리스트</SwiperTitleTypo>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              freeMode={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay, FreeMode, Pagination]}
+              className='mySwiper'
+              style={{ width: '320px', height: '400px' }}
+            >
+              {enterGoodsList.map((goods, index) => {
+                return (
+                  <SwiperSlide>
+                    <SwiperInnerContainer>
+                      <ItemCard {...goods} key={`goods_${index}`} />
+                    </SwiperInnerContainer>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </SwiperContainer>
+          <SwiperContainer>
+            <SwiperTitleTypo level={5}>
+              젠지 선수단 친필 사인 리스트
+            </SwiperTitleTypo>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              freeMode={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay, FreeMode, Pagination]}
+              className='mySwiper'
+              style={{ width: '320px', height: '400px' }}
+            >
+              {gengList.map((goods, index) => {
+                return (
+                  <SwiperSlide>
+                    <SwiperInnerContainer>
+                      <ItemCard {...goods} key={`goods_${index}`} />
+                    </SwiperInnerContainer>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </SwiperContainer>
+          <SwiperContainer>
+            <SwiperTitleTypo level={5}>
+              젠지 선수단 친필 사인 리스트
+            </SwiperTitleTypo>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              freeMode={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay, FreeMode, Pagination]}
+              className='mySwiper'
+              style={{ width: '320px', height: '400px' }}
+            >
+              {gengList.map((goods, index) => {
+                return (
+                  <SwiperSlide>
+                    <SwiperInnerContainer>
+                      <ItemCard {...goods} key={`goods_${index}`} />
+                    </SwiperInnerContainer>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </SwiperContainer>
+          <SwiperContainer>
+            <SwiperTitleTypo level={5}>니르바나 굿즈 리스트</SwiperTitleTypo>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              freeMode={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay, FreeMode, Pagination]}
+              className='mySwiper'
+              style={{ width: '320px', height: '400px' }}
+            >
+              {nirvanaList.map((goods, index) => {
+                return (
+                  <SwiperSlide>
+                    <SwiperInnerContainer>
+                      <ItemCard {...goods} key={`goods_${index}`} />
+                    </SwiperInnerContainer>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </SwiperContainer>
+          <SwiperContainer>
+            <SwiperTitleTypo level={5}>교환 상품 리스트</SwiperTitleTypo>
+            <Swiper
+              slidesPerView={1}
+              spaceBetween={30}
+              freeMode={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Autoplay, FreeMode, Pagination]}
+              className='mySwiper'
+              style={{ width: '320px', height: '400px' }}
+            >
+              {goodsList.map((goods, index) => {
+                return (
+                  <SwiperSlide>
+                    <SwiperInnerContainer>
+                      <ItemCard {...goods} key={`goods_${index}`} />
+                    </SwiperInnerContainer>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
+          </SwiperContainer>
+        </ContentContainer>
       </Container>
+      <FooterWrapper>
+        <MobileFooter />
+      </FooterWrapper>
     </Root>
   );
 };
--- src/pages/Main/Mobile/styled.ts
+++ src/pages/Main/Mobile/styled.ts
@@ -3,10 +3,13 @@ import styled from 'styled-components';
 
 export const Root = styled.div`
   width: 100%;
-  background: #eee;
+  min-height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
+  position: relative;
+  padding-top: 80px;
+  padding-bottom: 200px;
 `;
 
 export const Container = styled.div`
@@ -15,7 +18,6 @@ export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 20px;
-  padding-top: 100px;
 `;
 
 export const TitleTypo = styled(Typography.Title)``;
@@ -26,9 +28,45 @@ export const ContentContainer = styled.div`
   align-items: center;
   gap: 10px;
 `;
+export const BannerImg = styled.img`
+  width: 100%;
+  height: 300px;
+  object-fit: cover;
+  margin-top: -100px;
+  margin-bottom: 50px;
+`;
 
 export const ContentTypo = styled(Typography)``;
 
 export const ContentInput = styled(Input)``;
 
 export const ContentButton = styled(Button)``;
+
+export const SwiperContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-top: 50px;
+`;
+
+export const SwiperTitleTypo = styled(Typography.Title)`
+  &&& {
+    font-family: Jalnan;
+    font-weight: bold;
+  }
+`;
+
+export const SwiperInnerContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+`;
